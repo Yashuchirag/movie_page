9005f68017edab7a68fa078b8db0afc292a5ec18
Yashuchirag <chiragchandrashekar@gmail.com>
2025-09-10 09:47:36 -0700

    Updated logo info

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
-import React from 'react';
+import {useEffect} from 'react';
+
+const BASE_URL = process.env.REACT_APP_MOVIE_API_URL;
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        try {
+            const url = `${BASE_URL}&s=${(title)}`;
+            console.log("URL:", url);
+            const response = await fetch(url);
+      
+            if (!response.ok) {
+              throw new Error(`HTTP error! status: ${response.status}`);
+            }
+      
+            const data = await response.json();
+            console.log("Movie data:", data.Search);
+        } catch (err) {
+            console.error("Failed to fetch movies:", err);
+        }
+    };
+    useEffect(() => {
+        searchMovies('spiderman');
+    }, []);
+
     return (
         <div>
             <h1>Movie Page</h1>
